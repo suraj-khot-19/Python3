10e4500f5341599314cb6af89be19dbfc41d54a4
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-05-26 22:29:23 +0530

    for loops

---
+++ forloop.py
@@ -0,0 +1,19 @@
+# simp
+for i in range(1 , 10):
+    print(i)
+
+# str
+s1= "suraj"
+for i in range(0,len(s1)):
+    print(s1[i])
+
+# formating with print
+s2= "khot"
+for i in range(0,len(s2)):
+    print(f"{i} :{s2[i]}")
+
+# enhanced
+s3= "rowdy"
+for i in s3:
+    print(i)
+
