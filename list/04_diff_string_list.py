23b915e70661b96553edf1bde78e21e291129f42
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-05-27 16:19:35 +0530

    diffrence in string and list

---
+++ list/04_diff_string_list.py
@@ -0,0 +1,28 @@
+# string are immutable but list are mutable 
+ 
+str="suraj"
+
+print(str) # suraj
+
+print(str[2]) # r
+
+# str[2]="k"
+
+'''
+Traceback (most recent call last):
+  File "d:\python\list\04_diff_string_list.py", line 5, in <module>
+    str[2]="k"
+    ~~~^^^
+TypeError: 'str' object does not support item assignment
+'''
+
+
+list=[1,2,3]
+
+print(list)
+
+print(list[2])
+
+list[2]=10
+
+print(list)
