7b9dce4cac18cbcf506c3bb159be1f059ae68e1c
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-05-27 16:14:19 +0530

    list advantage to store diffrent kind of data

---
+++ list/03_adv_list.py
@@ -0,0 +1,14 @@
+#  can store int , float, string or any type of data in a single list
+data=["suraj","tcs",1.9,1,True]
+
+print(data) # ['suraj', 'tcs', 1.9, 1, True]
+
+
+print(type(data)) # <class 'list'>
+
+print(type(data[0]))  #<class 'str'>
+
+
+print(type(data[2])) # <class 'float'>
+
+print(type(data[4])) # <class 'bool'>
