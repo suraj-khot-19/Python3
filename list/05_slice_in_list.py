0b0defcca5ff2d9cc71784e0ab440eedc1f7a2a7
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-05-27 16:37:01 +0530

    update

--- list/05_slice_in_list.py
+++ list/05_slice_in_list.py
@@ -1,8 +1,8 @@
 # slicing [x:y] where y is excluded
 
-data=["suraj","tcs",1.9,1,True,"fucking life", "no hopes"]
+data=["suraj","tcs",1.9,1,True,"life", "hopes"]
 
-print(data) # data=["suraj","tcs",1.9,1,True,"fucking life", "no hopes"]
+print(data) # data=["suraj","tcs",1.9,1,True,"life", "hopes"]
 
 print(data[1:4]) # ['tcs', 1.9, 1]
 
