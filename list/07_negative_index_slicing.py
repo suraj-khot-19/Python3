b51735d19b81a599ef457473ba7098eb08e5f3f9
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-05-27 16:45:34 +0530

    negative index with slicing in list

---
+++ list/07_negative_index_slicing.py
@@ -0,0 +1,12 @@
+data=["suraj","tcs",1.9,1,True,"life", "hopes"]
+
+# list = [-4,-3,-2,-1]
+
+print(data[-4]) # [1]
+
+print(data[-4:-1]) # [1, True, 'life']
+
+print(data[:-3]) # ['suraj', 'tcs', 1.9, 1]
+
+print(data[-5:]) # [1.9, 1, True, 'life', 'hopes']
+
