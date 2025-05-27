a84a2b0b167b438605d3f6ee8a588d88c1b8171e
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-05-27 16:40:32 +0530

    advance slicing concept in py

---
+++ list/06_adv_slice_list.py
@@ -0,0 +1,13 @@
+# [:] start to end
+# [3:] index to end
+# [:9] start to index 
+
+data=["suraj","tcs",1.9,1,True,"life", "hopes"]
+
+print(data) # ['suraj', 'tcs', 1.9, 1, True, 'life', 'hopes']
+
+print(data[:]) # ['suraj', 'tcs', 1.9, 1, True, 'life', 'hopes']
+
+print(data[:5]) # ['suraj', 'tcs', 1.9, 1, True]
+
+print(data[4:]) # [True, 'life', 'hopes']
