14e60a9f52d7789693d2be46c8e5cf45878fc22d
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-05-29 17:38:43 +0530

    pop and remove method in list

---
+++ list/12_remove.py
@@ -0,0 +1,13 @@
+list=[1,2,3,4,5,6,7,8,9,10]
+
+list.remove(4)
+
+print(list)
+
+list1=[1,2,4,4,3,4,5,6,7,8,9,10]
+
+list1.remove(4)
+
+print(list1)
+
+#list1.remove(12) # list1.remove(12)
---
+++ list/13_pop.py
@@ -0,0 +1,11 @@
+list=[1,2,3,4,5,6,7,8,9,10] 
+
+print("before pop =>",list)
+
+list.pop()
+
+print("after pop =>",list)
+
+list.pop(3)
+
+print("after pop at index 3 =>",list)
