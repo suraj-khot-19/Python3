a7045d7852ead57ff3712b5db6eec971dcfcc6ee
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-05-29 17:09:04 +0530

    append vs extend method in list

---
+++ list/09_extend_vs_append.py
@@ -0,0 +1,15 @@
+list1=[1,2,3,4,5,6,7,8,9,10]
+
+list1.append([2,3])
+
+print("after append [2,3] => ",list1)
+
+
+list2=[1,2,3,4,5,6,7,8,9,10]
+
+list2.extend([2,3])
+
+print("after extend [2,3] => ",list2)
+
+
+print("append add list as it is but extends add list element into list")
