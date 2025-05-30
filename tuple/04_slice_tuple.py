74d807aea9bbe17a93813b79326700d2d2bada4c
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-05-30 18:58:02 +0530

    slicing and index method in tuple

---
+++ tuple/04_slice_tuple.py
@@ -0,0 +1,7 @@
+tuple=(1,2,3,4,5,6)
+
+print(tuple[2:3]) # (3,)
+
+print(tuple[:4]) # (1, 2, 3, 4)
+
+print(tuple[5:]) # (6,)
---
+++ tuple/05_tuple_index.py
@@ -0,0 +1,11 @@
+tuple=(4,5,6,7.8)
+
+# print(tuple.index(3))
+'''   
+print(tuple.index(3))
+       ~~~~~~~~~~~^^^
+ValueError: tuple.index(x): x not in tuple
+'''
+
+print(tuple.index(6))  # 2
+
