ed593b67aed8d9b4b9a0b4758e6403ec1b8c8376
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-05-30 19:10:53 +0530

    3 fav movie and store into a list

---
+++ assignment_1/02_3_fav_movie_list.py
@@ -0,0 +1,7 @@
+list=[]
+
+list.append(input("Enter your first fav movie :"))
+list.append(input("Enter your secound fav movie :"))
+list.append(input("Enter your third fav movie :"))
+
+print(list)
