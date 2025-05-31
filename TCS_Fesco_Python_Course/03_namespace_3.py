06d9c6783398e991af56605c00826a7028bf2b31
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-05-31 10:33:41 +0530

    namespace 3 task

---
+++ TCS_Fesco_Python_Course/03_namespace_3.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'Prompt' function below.
+#
+#
+
+def Prompt():
+    # Write your code here
+    x=input("Enter a STRING:\n")
+    print(x)
+    print(type(x))
+    
+
+if __name__ == '__main__':
+
+    Prompt()
